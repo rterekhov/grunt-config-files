7633d0c45df8a11ca72c1d7302ec4e7f34022887
Roman Terekhov <roman.terekhov@gmail.com>
2016-10-13 12:02:40 +0200

    Update example files

--- example/Gruntfile.js
+++ example/Gruntfile.js
@@ -2,4 +2,15 @@ module.exports = function(grunt) {
     'use strict';
 
     grunt.loadNpmTasks('grunt-config-files');
+
+    grunt.initConfig({
+        config: {
+            options: {
+                settingsFile:     'config/settings.js',
+                templateFiles:  [ 'app.config.template' ],
+                logSettings:      true,
+                logTemplateFiles: false
+            }
+        }
+    });
 };
